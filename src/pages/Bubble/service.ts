6cd0007a27224afc7ad259ed03a8cda932ded8e4
julianlliu <xliudaxia@163.com>
2023-10-04 21:05:13 +0800

    fix: 修复待办事项请求接口是首次不会携带token问题

--- src/pages/Bubble/service.ts
+++ src/pages/Bubble/service.ts
@@ -1,12 +1,19 @@
 import request from '@/utils/request';
 
 export async function queryBubble() {
-  return request('/api/v1/todo');
+  return request('/api/v1/todo',{
+    headers:{
+      'M-Token':`${localStorage.getItem('M-Token')}`
+    }
+  });
 }
 
 export async function triggerBubble(id: number, status: boolean) {
   return request('/api/v1/todo/' + `${id}`, {
     method: 'PUT',
+    headers:{
+      'M-Token':`${localStorage.getItem('M-Token')}`
+    },
     data: {
       status: !status,
     },
@@ -16,12 +23,18 @@ export async function triggerBubble(id: number, status: boolean) {
 export async function deleteBubble(id: number) {
   return request('/api/v1/todo/' + `${id}`, {
     method: 'DELETE',
+    headers:{
+      'M-Token':`${localStorage.getItem('M-Token')}`
+    }
   });
 }
 
 export async function addBubble(content: string) {
   return request('/api/v1/todo', {
     method: 'POST',
+    headers:{
+      'M-Token':`${localStorage.getItem('M-Token')}`
+    },
     data: {
       title: content,
     },
